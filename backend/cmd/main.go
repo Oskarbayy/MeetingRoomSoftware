25ccde41c75455bb268550b8a828f54844207212
Oskar Hansen <oskarbayy@gmail.com>
2024-12-20 11:53:45 +0100

    v0.0.1 release some bugs left to fix

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"backend/pkg/api"
 	"backend/pkg/serialhandler"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	config, err := serialhandler.LoadConfig("config.json")
+	// Parse the config path from command-line arguments
+	configPath := flag.String("config", "", "Path to the configuration file")
+	flag.Parse()
+
+	// Default to the server's directory if no path is provided
+	if *configPath == "" {
+		executablePath, err := os.Executable()
+		if err != nil {
+			log.Fatalf("Failed to get executable path: %v", err)
+		}
+		executableDir := filepath.Dir(executablePath)
+		*configPath = filepath.Join(executableDir, "config.json")
+	}
+
+	// Load the config
+	config, err := serialhandler.LoadConfig(*configPath)
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
@@ -30,9 +49,9 @@ func main() {
 	log.Println("Running startup commands...")
 	port.RunStartupCommands(config.StartupCommands)
 
-	log.Println("Server is listening on port :8080...")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	portStr := strconv.Itoa(config.ServerPort) // Convert integer to string
+	log.Println("Server is listening on port", portStr)
+	if err := http.ListenAndServe(":"+portStr, router); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
-
 }
